969f3ed5408c8d64c5c479e633e0d615b0c9fc2f
Collin Gilmore <collingilmore@gmail.com>
2020-05-14 16:50:04 -0700

    Updates

---
+++ precache-manifest.5659e5770a457027b0c5e28883e641ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f504a71300e632833c629142cc1ca813",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "8785202c0feb66e02fe7",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a400bc9a75a6e8af6732",
+    "url": "/Robofriends/static/css/main.9b0a90cc.chunk.css"
+  },
+  {
+    "revision": "8785202c0feb66e02fe7",
+    "url": "/Robofriends/static/js/2.19a02f26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.19a02f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a400bc9a75a6e8af6732",
+    "url": "/Robofriends/static/js/main.a7cb6cc4.chunk.js"
+  },
+  {
+    "revision": "d5eda6d00e5b01ec3b63",
+    "url": "/Robofriends/static/js/runtime-main.2dcc84d0.js"
+  }
+]);
